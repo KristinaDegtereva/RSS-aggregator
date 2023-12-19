836310ec43422471985040898b7c574818fe2c80
Kristina Degtereva <kristalluna@yandex.ru>
2023-12-19 15:57:49 +0300

    2 step

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import './styles.scss';
 import 'bootstrap';
+import keyBy from 'lodash/keyBy.js';
 import * as yup from 'yup';
 import axios from 'axios';
 import onChange from 'on-change';
 
 
 export default () => {
+
+  const state = {
+    urls: [],
+  };
+
   const form = document.querySelector('.rss-form');
   const input = document.querySelector('#url-input');
   const feedback = document.querySelector('.feedback')
@@ -21,13 +27,15 @@ export default () => {
     const schema = yup.object().shape({
       url: yup.string()
       .url('Ссылка должна быть валидным URL')
+      .notOneOf(state.urls, 'RSS уже существует')
     });
 
     schema.validate(data)
-  .then()
-  .catch((ValidationError) => {
+    .then((data) => state.urls.push(data.url))
+    .catch((ValidationError) => {
+    console.log(ValidationError.value)
     feedback.textContent = ValidationError.errors;
-    input.classList.add('is-invalid')
+    input.classList.add('is-invalid');
   });
   })
 }
