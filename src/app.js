af347119fba98248d5b713ddd32f8a2f09587286
Kristina Degtereva <kristalluna@yandex.ru>
2024-01-01 18:01:00 +0300

    4 step

--- src/app.js
+++ src/app.js
@@ -76,34 +76,7 @@ export default () => {
             throw new Error('Network response was not ok.')
           })
           .then((data) => {
-            // console.log(data.contents);
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(data.contents, 'text/xml');
-            const feedTitle = doc.querySelector('title').textContent;
-            const feedDescription = doc.querySelector('description').textContent;
-            const items = doc.querySelectorAll('item');
-            const posts = [];
-            items.forEach((item) => {
-              const title = item.querySelector('title').textContent;
-              const description = item.querySelector('description').textContent;
-              const link = item.querySelector('link').textContent;
-              const pubDate = item.querySelector('pubDate').textContent;
-              posts.push({
-                title,
-                description,
-                link,
-                pubDate,
-              });
-            });
-            watchState.form.parsed = {
-              feed: {
-                url,
-                title: feedTitle,
-                description: feedDescription,
-              },
-              posts,
-            };
-            watchState.form.processState = 'success';
+            parse(data, watchState, url);
           })
       })
       .catch((e) => {
