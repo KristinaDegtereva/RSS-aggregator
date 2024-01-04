f6b2475574bac1b802d3e39f88199b5c4b9a59be
Kristina Degtereva <kristalluna@yandex.ru>
2024-01-04 15:31:45 +0300

    4 step

--- src/app.js
+++ src/app.js
@@ -21,23 +21,6 @@ export default () => {
     },
   };
 
-  const i18n = i18next.createInstance();
-  i18n.init({
-    lng: 'ru',
-    debug: false,
-    resources,
-  });
-
-  yup.setLocale({
-    mixed: {
-      notOneOf: () => ({ key: 'dublicateError' }),
-    },
-    string: {
-      url: () => ({ key: 'urlError' }),
-    },
-  });
-
-
   const elements = {
     form: document.querySelector('.rss-form'),
     input: document.querySelector('#url-input'),
@@ -47,41 +30,58 @@ export default () => {
     posts: document.querySelector('.posts')
   };
 
-  const watchState = onChange(state, initView(elements, i18n, state));
-
-  elements.form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const value = (formData.get('url')).trim();
-    watchState.form.field.url = value;
-
-    const urls = state.form.allUrls.map(({ url }) => url);
-    const schema = yup.string()
-      .url()
-      .notOneOf(urls);
-
-    const validated = (url) => {
-      watchState.form.allUrls.push({ url, feedId: _.uniqueId() });
-      watchState.form.field.url = '';
-      watchState.form.processState = 'sending';
-      watchState.form.error = {};
-    }
-
-    schema.validate(value)
-      .then((url) => {
-        validated(url);
-        fetch(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`)
-          .then(response => {
-            if (response.ok) return response.json()
-            throw new Error('Network response was not ok.')
-          })
-          .then((data) => {
-            parse(data, watchState, url);
-          })
-      })
-      .catch((e) => {
-        watchState.form.error = e.message;
-        return _.keyBy(e.inner, 'path');
-      })
+  const i18n = i18next.createInstance();
+  i18n.init({
+    lng: 'ru',
+    debug: false,
+    resources,
+  })
+  .then(() => {
+    yup.setLocale({
+      mixed: {
+        notOneOf: () => ({ key: 'dublicateError' }),
+      },
+      string: {
+        url: () => ({ key: 'urlError' }),
+      },
+    });
+  
+    const watchState = onChange(state, initView(elements, i18n, state));
+  
+    elements.form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const formData = new FormData(event.target);
+      const value = (formData.get('url')).trim();
+      watchState.form.field.url = value;
+  
+      const urls = state.form.allUrls.map(({ url }) => url);
+      const schema = yup.string()
+        .url()
+        .notOneOf(urls);
+  
+      const successAdd = (url) => {
+        watchState.form.allUrls.push({ url, feedId: _.uniqueId() });
+        watchState.form.field.url = '';
+        watchState.form.processState = 'sending';
+        watchState.form.error = {};
+      }
+  
+      schema.validate(value)
+        .then((url) => {
+          successAdd(url);
+          fetch(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`)
+            .then(response => {
+              if (response.ok) return response.json()
+              throw new Error('Network response was not ok.')
+            })
+            .then((data) => {
+              parse(data, watchState, url);
+            })
+        })
+        .catch((e) => {
+          watchState.form.error = e.message;
+          return _.keyBy(e.inner, 'path');
+        })
+    })
   })
 };
