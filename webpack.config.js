76144bbb103fe6888c98a2fad42ecbf802998169
k.degtereva <k.degtereva@MAC1907.local>
2024-01-26 14:39:45 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,13 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-import path from 'path';
+// import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-const config = {
+export default {
   mode: process.env.NODE_ENV || 'development',
-  entry: path.resolve(__dirname, './src/index.js'),
+  entry: './src/index.js',
   output: {
-    // path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   devServer: {
     open: true,
@@ -19,39 +17,30 @@ const config = {
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
-
   ],
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/i,
+        test: /.(js|jsx)$/i,
         loader: 'babel-loader',
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        test: /.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
-      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      { test: /.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
-        test: /\.scss$/,
+        test: /.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
       },
       {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        test: /.woff2?(\?v=[0-9].[0-9].[0-9])?$/,
         use: 'url-loader?limit=10000',
       },
       {
-        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        test: /.(ttf|eot|svg)(\?[\s\S]+)?$/,
         use: 'file-loader',
       },
     ],
   },
 };
-
-if (isProduction) {
-  config.mode = 'production';
-} else {
-  config.mode = 'development';
-}
-
-export default config;
